57f1fc9999020c1b378376eb51777216ed1714da
ikoanymg <ikoanymg@gmail.com>
2023-08-01 23:49:14 +0900

    favoriteのcontroller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,24 @@
 class FavoritesController < ApplicationController
+  before_action :set_post
 
   def create
-    @post = Post.find(params[:post_id])
-    @post.favorites.create(user: current_user)
-  
+    @favorite = @post.favorites.create(user: current_user)
     respond_to do |format|
       format.turbo_stream
     end
+  end
+
+  def destroy
+    @favorite = @post.favorites.find_by(user: current_user)
+    @favorite.destroy
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
