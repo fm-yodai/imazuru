6a5bd3e2c10bcbd16e0a5f30c43291bafe89f566
kakifried <3peace.18@gmail.com>
2023-08-02 00:03:54 +0900

    一時コミット

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,7 @@ class FavoritesController < ApplicationController
   def create
     @favorite = @post.favorites.create(user: current_user)
     respond_to do |format|
-      format.turbo_stream
+      format.js
     end
   end
 
@@ -12,7 +12,7 @@ class FavoritesController < ApplicationController
     @favorite = @post.favorites.find_by(user: current_user)
     @favorite.destroy
     respond_to do |format|
-      format.turbo_stream
+      format.js
     end
   end
 
